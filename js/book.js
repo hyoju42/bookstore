b3076736d4405dcdb309b3f84e0dbba71e9702a0
hyoju42 <hj_hyoju42@naver.com>
2026-09-02 10:25:36 +0900

    2026.09.01

--- js/book.js
+++ js/book.js
@@ -1,48 +1,80 @@
-
-async function bookData() {
+// 출판사 추천
+async function fetchBooks(query) {
     const REST_API_KEY = '672a3e50542b7d6b98430b8a29dd28ab';
     const params = new URLSearchParams({
-        query: '미움받을 용기',
+        query,
         target: 'title',
-        size: 50  // 한 페이지에 보여질 문서 수
+        size: 20
     })
     console.log(params);
 
-    try {
-        const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
-            // 옵션 설정
-            method: 'GET',
-            headers: {
-                Authorization: `KakaoAK ${REST_API_KEY}`
-            }
-        });
-        console.log(response);  // Response객체 반환
-
-        if (!response.ok) {
-            throw new Error("HTTP오류 - 상태코드 : " + response.status);
+    const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+        method: 'GET',
+        headers: {
+            Authorization: `KakaoAK ${REST_API_KEY}`
         }
-
-        const data = await response.json();
-
-        const boxElements = document.querySelectorAll(".box");
-
-        boxElements.forEach((box, i) => {
-            const doc = data.documents[i];
-
-            if (!doc) return;
-
-            box.innerHTML = `<img src="${data.documents[i].thumbnail}">
-                    <h3>${data.documents[i].title}</h3>
-                    <h6>${data.documents[i].authors}</h6>
-                    <p>${data.documents[i].contents.substring(0, 60)}</p>
-                    <button>click</button>
-                    `
-        });
-
-    } catch (error) {
-        console.log('에러 발생', error);
+    });
+    if (!response.ok) {
+        throw new Error("HTTP오류 - 상태코드 : " + response.status);
     }
+    return response.json();
+}
 
+async function bookData() {
+    try {
+        // query와 section ID를 매핑
+        const queries = [
+            { query: "장편소설", sectionId: "gobobox" },
+            { query: "오늘", sectionId:"swiper2"},
+            { query: "양장", sectionId: "swiper4"},
+            { query: "트렌드", sectionId: "swiper5" },
+            { query: "예능", sectionId: "swiper6" },
+            { query: "교보문고", sectionId: "swiper9" },
+            { query: "베스트셀러", sectionId: "peoplebox" },
+            { query: "탐험", sectionId: "aibox" },
+            { query: "기쁨", sectionId: "aipickbox"},
+            { query: "출판", sectionId: "swiper14" },
+            { query: "전체", sectionId: "tab1"},
+            { query: "한국소설", sectionId: "tab2"},
+            { query: "해외소설", sectionId: "tab3"},
+            { query: "eBook", sectionId: "tab4"},
+            { query: "삶", sectionId: "tab5"},
+            { query: "hot", sectionId: "tab6"},
+            { query: "교보", sectionId: "tab7"},
+            { query: "국내", sectionId: "tab8"},
+            { query: "외국", sectionId: "tab9"},
+        ];
+
+        for (const { query, sectionId } of queries) {
+            const data = await fetchBooks(query);
+
+            const section = document.querySelector(`#${sectionId}`);
+            const boxElements = section.querySelectorAll(".box");
+
+            boxElements.forEach((box, i) => {
+                const doc = data.documents[i];
+                if (!doc) return;
+
+                box.innerHTML = `<img src="${doc.thumbnail}">
+                        <p>${doc.title}</p>
+                        `
+            });
+        }
+    } catch (error) {
+        console.error('에러 발생:', error);
+    }
 }
 
 bookData();
+
+const tabItems = document.querySelectorAll('.booktab li');
+const tabs = document.querySelectorAll('article');
+
+tabItems.forEach((tab, i) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach((tab, j) => {
+            tab.style.display = (i === j) ? 'flex' : 'none';
+        });
+    });
+});
+
--- js/swiper.js
+++ js/swiper.js
@@ -1,39 +1,150 @@
+// 슬라이더1_1
+var swiper = new Swiper('.swiper1_1', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
 
-const progressCircle = document.querySelector('.autoplay-progress svg');
-const progressContent = document.querySelector('.autoplay-progress span');
-var swiper = new Swiper('.slider1_1', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '#slider1_1 .swiper-button-next',
-        prevEl: '#slider1_1 .swiper-button-prev',
-    },
-    on: {
-        autoplayTimeLeft(s, time, progress) {
-            progressCircle.style.setProperty('--progress', 1 - progress);
-            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+// 슬라이더1_2
+var swiper = new Swiper('.swiper1_2', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
         },
-    },
-});
-var swiper = new Swiper('.slider1_2', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    pagination: {
-        el: '#slider1_2 .swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '#slider1_1 .swiper-button-next',
-        prevEl: '#slider1_1 .swiper-button-prev',
-    },
-    on: {
-        autoplayTimeLeft(s, time, progress) {
-            progressCircle.style.setProperty('--progress', 1 - progress);
-            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      });
+
+// 오늘의 선택
+var swiper = new Swiper('.swiper2', {
+        slidesPerView: 8,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
-    },
-});
+      });
+
+// MD선택
+var swiper = new Swiper('.swiper4', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// 트렌드
+var swiper = new Swiper('.swiper5', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// 북 퍼포먼스
+var swiper = new Swiper('.swiper6', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// 나만의 취향
+var swiper = new Swiper('.swiper8', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// 교보문고 만듦
+var swiper = new Swiper('.swiper9', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+
+
+// 바로출판
+var swiper = new Swiper('.swiper14', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// 교보문고에서만
+var swiper = new Swiper('.swiper15', {
+        slidesPerView: 6,
+        slidesPerGroup: 6,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// CASTing
+var swiper = new Swiper('.swiper16', {
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+// event
+
+var eventtitle = ['교보문고', 'eBook','핫트랙스'];
+var swiper = new Swiper('.swiper17', {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 9,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + eventtitle[index] + '</span>';
+          },
+        },
+      });
